5a9573cf19b09e9e027ba775bda815b81829a6ae
esevre <esevre@gmail.com>
2020-03-25 17:16:50 +0900

    Basic crawling capability. It will download all links from a single site, then download what they are pointing at. Also deleted unused methods

--- main.cpp
+++ main.cpp
@@ -17,12 +17,40 @@
 #include "ES/web/ParsedUrl.hpp"
 
 
-void write_file(const std::string &content, const std::string &filename)
+void write_html(const ES::web::ParsedUrl &url,
+                const std::string &base_path="",
+                const bool verbose = false)
 {
-    std::ofstream outfile(filename);
-    outfile << content;
-}
+    try {
+        // create a filename:
+        std::string full_path;
+        if (!base_path.empty() && base_path.back() != '/'){
+            full_path = base_path + "/" + url.subdirectory();
+        } else {
+            full_path = base_path + url.subdirectory();
+        }
+        std::string filename;
+        if (url.page().empty()) {
+            filename = full_path + "index.html";
+        } else {
+            filename = full_path + url.page();
+        }
 
+        if (verbose){
+            std::cout << "downloading page: " << url.whole_url() << std::endl;
+        }
+        auto result = ES::web::get_html(url.authority(), url.path_to_page(), "80");
+        if (verbose){
+            std::cout << "writing page: " << filename << std::endl;
+        }
+        std::filesystem::create_directories(full_path);
+        std::ofstream out(filename);
+        out << result;
+    } catch (std::exception &e) {
+        std::cout << "failed to get: " << url.whole_url() << '\n';
+        std::cout << "error: " << e.what() << std::endl;
+    }
+}
 
 
 
@@ -32,7 +60,7 @@ int main(int argc, char *argv[]) {
     }
     std::string base_site = std::string(argv[1]);
     std::string base_dir = "Site";
-    if (argc < 3) {
+    if (argc > 2) {
         base_dir = argv[2];
     }
     std::cout << "base_dir:  " << base_dir << std::endl;
@@ -42,16 +70,12 @@ int main(int argc, char *argv[]) {
     std::cout << "************************************************\n";
 
     std::vector<std::string> tag_list = ES::web::get_tags_with(result, "href");
-
     std::vector<ES::web::ParsedUrl> url_to_visit;
-
     std::cout << "Found " << tag_list.size() << " tags\n";
 
     for (auto &tag : tag_list)
     {
         auto tag_url = ES::web::extract_id_from_tag(tag, "href");
-//        std::cout << "extract tag: " << tag << '\n';
-//        std::cout << "grab test: " << tag_url << '\n';
         auto blarg = ES::web::ParsedUrl(tag_url);
         if (blarg.authority().empty()) {
             blarg.authority(base_site);
@@ -59,19 +83,19 @@ int main(int argc, char *argv[]) {
         url_to_visit.push_back(blarg);
     }
 
-    std::cout << "*********************************************************\n";
-    std::cout << "**                 TESTING URL OBJECTS                 **\n";
-    std::cout << "*********************************************************\n";
-    for (const auto &url : url_to_visit) {
-        std::cout << "*********************************************************\n";
-        ES::web::test_url(url);
-        std::cout << "*********************************************************\n";
-    }
+//    std::cout << "*********************************************************\n";
+//    std::cout << "**                 TESTING URL OBJECTS                 **\n";
+//    std::cout << "*********************************************************\n";
+//    for (const auto &url : url_to_visit) {
+//        std::cout << "*********************************************************\n";
+//        ES::web::test_url(url);
+//        std::cout << "*********************************************************\n";
+//    }
 
-    std::filesystem::create_directories("../Site/path/here");
-    std::ofstream out("../Site/path/here/thing.txt");
-//    out << std::string("This is some stuff\nmore stuff");
-    out << result;
-    out.close();
+    for (auto &url : url_to_visit){
+        if (url.second_level_domain() == "interridge") {
+            write_html(url, "../Site", true);
+        }
+    }
 }
 
